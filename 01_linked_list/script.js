4c2308f798e40e3d6c8e36373b348bf2cbd9a012
Albert Goncalves <goncalves.albert@gmail.com>
2020-02-17 23:52:39 -0500

    anon function boundaries, no-op

--- 01_linked_list/script.js
+++ 01_linked_list/script.js
@@ -10,11 +10,11 @@ function main() {
     var nodeHalfWidth = nodeWidth / 2;
     var canvasHalfHeight = canvas.height / 2;
     var canvasWidthSlice = canvas.width / n;
-    var i;
-    {
+    (function() {
+        /* Initialize `nodes` */
         var k = 15;
         var kHalf = k / 2;
-        for (i = 0; i < n; i++) {
+        for (var i = 0; i < n; i++) {
             var xOrigin = (canvasWidthSlice * (i + 0.5)) - nodeHalfWidth;
             nodes[i] = {
                 xOrigin: xOrigin,
@@ -23,74 +23,73 @@ function main() {
                     kHalf,
             };
         }
-    }
-    {
+    })();
+    (function() {
+        /* Draw everything */
         var ctx = canvas.getContext("2d");
         ctx.imageSmoothingEnabled = true;
         var color = "hsl(0, 0%, 20%)";
         ctx.fillStyle = color;
         ctx.strokeStyle = color;
         ctx.lineWidth = 1.65;
-        {
+        (function() {
+            /* Draw `nodes` surroundings */
             var yOver = canvasHalfHeight - nodeHeight;
             var yUnder = canvasHalfHeight + nodeHeight;
             ctx.setLineDash([2.25, 3]);
-            {
-                var canvasPadWidth = 10;
-                ctx.strokeRect(canvasPadWidth, yUnder,
-                               canvasWidthSlice - (canvasPadWidth * 2),
-                               yOver - yUnder);
-            }
+            var canvasPadWidth = 10;
+            ctx.strokeRect(canvasPadWidth, yUnder,
+                           canvasWidthSlice - (canvasPadWidth * 2),
+                           yOver - yUnder);
             ctx.beginPath();
-            for (i = 1; i < n; i++) {
+            for (var i = 1; i < n; i++) {
                 var node = nodes[i];
-                var xOriginWidth = node.xOrigin + nodeWidth;
+                var xOriginOffset = node.xOrigin + nodeWidth;
                 ctx.moveTo(node.xOrigin, yOver);
-                ctx.lineTo(xOriginWidth, yOver);
+                ctx.lineTo(xOriginOffset, yOver);
                 ctx.moveTo(node.xOrigin, yUnder);
-                ctx.lineTo(xOriginWidth, yUnder);
+                ctx.lineTo(xOriginOffset, yUnder);
             }
             ctx.stroke();
-        }
-        var nodeFrom;
-        {
+        })();
+        (function() {
+            /* Draw `nodes` rects and pointers */
             var radius = 2.5;
-            var piDouble = Math.PI * 2;
+            var twicePi = Math.PI * 2;
             var nodeThreeQuarterWidth = nodeWidth * 0.75;
             ctx.setLineDash([]);
             ctx.beginPath();
-            for (i = 1; i < n; i++) {
-                nodeFrom = nodes[i - 1];
+            for (var i = 1; i < n; i++) {
+                var nodeFrom = nodes[i - 1];
                 var nodeTo = nodes[i];
                 var xFrom = nodeFrom.x + nodeThreeQuarterWidth;
                 var yFrom = nodeFrom.y + nodeHalfHeight;
                 var yTo = nodeTo.y + nodeHalfHeight;
-                {
-                    ctx.strokeRect(nodeFrom.x, nodeFrom.y, nodeWidth,
-                                   nodeHeight);
-                    ctx.moveTo(xFrom + radius, yFrom);
-                    ctx.arc(xFrom, yFrom, radius, 0, piDouble);
-                    ctx.moveTo(nodeTo.x + radius, yTo);
-                    ctx.arc(nodeTo.x, yTo, radius, 0, piDouble);
-                    ctx.moveTo(xFrom, yFrom);
-                    ctx.lineTo(nodeTo.x, yTo);
-                }
+                ctx.strokeRect(nodeFrom.x, nodeFrom.y, nodeWidth, nodeHeight);
+                ctx.moveTo(xFrom + radius, yFrom);
+                ctx.arc(xFrom, yFrom, radius, 0, twicePi);
+                ctx.moveTo(nodeTo.x + radius, yTo);
+                ctx.arc(nodeTo.x, yTo, radius, 0, twicePi);
+                ctx.moveTo(xFrom, yFrom);
+                ctx.lineTo(nodeTo.x, yTo);
             }
             ctx.stroke();
             ctx.fill();
-        }
-        {
+        })();
+        (function() {
+            /* Draw `nodes` separator details */
             ctx.setLineDash([1, 2]);
             ctx.beginPath();
-            for (i = 1; i < n; i++) {
-                nodeFrom = nodes[i - 1];
-                var xSep = nodeFrom.x + nodeHalfWidth;
-                ctx.moveTo(xSep, nodeFrom.y);
-                ctx.lineTo(xSep, nodeFrom.y + nodeHeight);
+            var m = n - 1;
+            for (var i = 0; i < m; i++) {
+                var node = nodes[i];
+                var xSep = node.x + nodeHalfWidth;
+                ctx.moveTo(xSep, node.y);
+                ctx.lineTo(xSep, node.y + nodeHeight);
             }
             ctx.stroke();
-        }
-    }
+        })();
+    })();
 }
 
 main();
