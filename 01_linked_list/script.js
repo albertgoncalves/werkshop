d2cff5dd47041b12d92af1daa8a18d9d037cfedb
Albert Goncalves <goncalves.albert@gmail.com>
2020-03-02 23:39:47 -0500

    fully ts now

--- 01_linked_list/script.js
+++ 01_linked_list/script.js
@@ -1,95 +1,141 @@
-"use strict";
+(function () {
+    var defines = {};
+    var entry = [null];
+    function define(name, dependencies, factory) {
+        defines[name] = { dependencies: dependencies, factory: factory };
+        entry[0] = name;
+    }
+    define("require", ["exports"], function (exports) {
+        Object.defineProperty(exports, "__cjsModule", { value: true });
+        Object.defineProperty(exports, "default", { value: function (name) { return resolve(name); } });
+    });
+    "use strict";
+    function main() {
+        var canvas = document.getElementById("canvas");
+        var n = (Math.floor(Math.random() * 4) + 3);
+        var nodes = new Array(n);
+        var nodeHeight = 45;
+        var nodeWidth = 60;
+        var nodeHalfHeight = nodeHeight / 2;
+        var nodeHalfWidth = nodeWidth / 2;
+        var canvasHalfHeight = canvas.height / 2;
+        var canvasWidthSlice = canvas.width / n;
+        (function () {
+            var k = 15;
+            var kHalf = k / 2;
+            for (var i = 0; i < n; i++) {
+                var xOrigin = (canvasWidthSlice * (i + 0.5)) - nodeHalfWidth;
+                nodes[i] = {
+                    xOrigin: xOrigin,
+                    x: xOrigin + (Math.random() * k) - kHalf,
+                    y: canvasHalfHeight - nodeHalfHeight + (Math.random() * k) -
+                        kHalf
+                };
+            }
+        })();
+        (function () {
+            var ctx = canvas.getContext("2d");
+            ctx.imageSmoothingEnabled = true;
+            var color = "hsl(0, 0%, 20%)";
+            ctx.fillStyle = color;
+            ctx.strokeStyle = color;
+            ctx.lineWidth = 1.65;
+            (function () {
+                var yOver = canvasHalfHeight - nodeHeight;
+                var yUnder = canvasHalfHeight + nodeHeight;
+                ctx.setLineDash([2.25, 3]);
+                var canvasPadWidth = 10;
+                ctx.strokeRect(canvasPadWidth, yUnder, canvasWidthSlice - (canvasPadWidth * 2), yOver - yUnder);
+                ctx.beginPath();
+                for (var i = 1; i < n; i++) {
+                    var node = nodes[i];
+                    var xOriginOffset = node.xOrigin + nodeWidth;
+                    ctx.moveTo(node.xOrigin, yOver);
+                    ctx.lineTo(xOriginOffset, yOver);
+                    ctx.moveTo(node.xOrigin, yUnder);
+                    ctx.lineTo(xOriginOffset, yUnder);
+                }
+                ctx.stroke();
+            })();
+            (function () {
+                var radius = 2.5;
+                var twicePi = Math.PI * 2;
+                var nodeThreeQuarterWidth = nodeWidth * 0.75;
+                ctx.setLineDash([]);
+                ctx.beginPath();
+                for (var i = 1; i < n; i++) {
+                    var nodeFrom = nodes[i - 1];
+                    var nodeTo = nodes[i];
+                    var xFrom = nodeFrom.x + nodeThreeQuarterWidth;
+                    var yFrom = nodeFrom.y + nodeHalfHeight;
+                    var yTo = nodeTo.y + nodeHalfHeight;
+                    ctx.strokeRect(nodeFrom.x, nodeFrom.y, nodeWidth, nodeHeight);
+                    ctx.moveTo(xFrom + radius, yFrom);
+                    ctx.arc(xFrom, yFrom, radius, 0, twicePi);
+                    ctx.moveTo(nodeTo.x + radius, yTo);
+                    ctx.arc(nodeTo.x, yTo, radius, 0, twicePi);
+                    ctx.moveTo(xFrom, yFrom);
+                    ctx.lineTo(nodeTo.x, yTo);
+                }
+                ctx.stroke();
+                ctx.fill();
+            })();
+            (function () {
+                ctx.setLineDash([1, 2]);
+                ctx.beginPath();
+                var m = n - 1;
+                for (var i = 0; i < m; i++) {
+                    var node = nodes[i];
+                    var xSep = node.x + nodeHalfWidth;
+                    ctx.moveTo(xSep, node.y);
+                    ctx.lineTo(xSep, node.y + nodeHeight);
+                }
+                ctx.stroke();
+            })();
+        })();
+    }
+    main();
+    
+    'marker:resolver';
 
-function main() {
-    var canvas = document.getElementById("canvas");
-    var n = (Math.floor(Math.random() * 4) + 3);
-    var nodes = new Array(n);
-    var nodeHeight = 45;
-    var nodeWidth = 60;
-    var nodeHalfHeight = nodeHeight / 2;
-    var nodeHalfWidth = nodeWidth / 2;
-    var canvasHalfHeight = canvas.height / 2;
-    var canvasWidthSlice = canvas.width / n;
-    (function() {
-        /* NOTE: Initialize `nodes` */
-        var k = 15;
-        var kHalf = k / 2;
-        for (var i = 0; i < n; i++) {
-            var xOrigin = (canvasWidthSlice * (i + 0.5)) - nodeHalfWidth;
-            nodes[i] = {
-                xOrigin: xOrigin,
-                x: xOrigin + (Math.random() * k) - kHalf,
-                y: canvasHalfHeight - nodeHalfHeight + (Math.random() * k) -
-                    kHalf,
-            };
+    function get_define(name) {
+        if (defines[name]) {
+            return defines[name];
         }
-    })();
-    (function() {
-        /* NOTE: Draw everything */
-        var ctx = canvas.getContext("2d");
-        ctx.imageSmoothingEnabled = true;
-        var color = "hsl(0, 0%, 20%)";
-        ctx.fillStyle = color;
-        ctx.strokeStyle = color;
-        ctx.lineWidth = 1.65;
-        (function() {
-            /* NOTE: Draw `nodes` surroundings */
-            var yOver = canvasHalfHeight - nodeHeight;
-            var yUnder = canvasHalfHeight + nodeHeight;
-            ctx.setLineDash([2.25, 3]);
-            var canvasPadWidth = 10;
-            ctx.strokeRect(canvasPadWidth, yUnder,
-                           canvasWidthSlice - (canvasPadWidth * 2),
-                           yOver - yUnder);
-            ctx.beginPath();
-            for (var i = 1; i < n; i++) {
-                var node = nodes[i];
-                var xOriginOffset = node.xOrigin + nodeWidth;
-                ctx.moveTo(node.xOrigin, yOver);
-                ctx.lineTo(xOriginOffset, yOver);
-                ctx.moveTo(node.xOrigin, yUnder);
-                ctx.lineTo(xOriginOffset, yUnder);
-            }
-            ctx.stroke();
-        })();
-        (function() {
-            /* NOTE: Draw `nodes` rects and pointers */
-            var radius = 2.5;
-            var twicePi = Math.PI * 2;
-            var nodeThreeQuarterWidth = nodeWidth * 0.75;
-            ctx.setLineDash([]);
-            ctx.beginPath();
-            for (var i = 1; i < n; i++) {
-                var nodeFrom = nodes[i - 1];
-                var nodeTo = nodes[i];
-                var xFrom = nodeFrom.x + nodeThreeQuarterWidth;
-                var yFrom = nodeFrom.y + nodeHalfHeight;
-                var yTo = nodeTo.y + nodeHalfHeight;
-                ctx.strokeRect(nodeFrom.x, nodeFrom.y, nodeWidth, nodeHeight);
-                ctx.moveTo(xFrom + radius, yFrom);
-                ctx.arc(xFrom, yFrom, radius, 0, twicePi);
-                ctx.moveTo(nodeTo.x + radius, yTo);
-                ctx.arc(nodeTo.x, yTo, radius, 0, twicePi);
-                ctx.moveTo(xFrom, yFrom);
-                ctx.lineTo(nodeTo.x, yTo);
-            }
-            ctx.stroke();
-            ctx.fill();
-        })();
-        (function() {
-            /* NOTE: Draw `nodes` separator details */
-            ctx.setLineDash([1, 2]);
-            ctx.beginPath();
-            var m = n - 1;
-            for (var i = 0; i < m; i++) {
-                var node = nodes[i];
-                var xSep = node.x + nodeHalfWidth;
-                ctx.moveTo(xSep, node.y);
-                ctx.lineTo(xSep, node.y + nodeHeight);
-            }
-            ctx.stroke();
-        })();
-    })();
-}
-
-main();
+        else if (defines[name + '/index']) {
+            return defines[name + '/index'];
+        }
+        else {
+            var dependencies = ['exports'];
+            var factory = function (exports) {
+                try {
+                    Object.defineProperty(exports, "__cjsModule", { value: true });
+                    Object.defineProperty(exports, "default", { value: require(name) });
+                }
+                catch (_a) {
+                    throw Error(['module "', name, '" not found.'].join(''));
+                }
+            };
+            return { dependencies: dependencies, factory: factory };
+        }
+    }
+    var instances = {};
+    function resolve(name) {
+        if (instances[name]) {
+            return instances[name];
+        }
+        if (name === 'exports') {
+            return {};
+        }
+        var define = get_define(name);
+        instances[name] = {};
+        var dependencies = define.dependencies.map(function (name) { return resolve(name); });
+        define.factory.apply(define, dependencies);
+        var exports = dependencies[define.dependencies.indexOf('exports')];
+        instances[name] = (exports['__cjsModule']) ? exports["default"] : exports;
+        return instances[name];
+    }
+    if (entry[0] !== null) {
+        return resolve(entry[0]);
+    }
+})();
---
+++ 01_linked_list/src/main.ts
@@ -0,0 +1,104 @@
+"use strict";
+
+interface Point {
+    xOrigin: number;
+    x: number;
+    y: number;
+}
+
+function main() {
+    const canvas: HTMLCanvasElement =
+        document.getElementById("canvas") as HTMLCanvasElement;
+    const n: number = (Math.floor(Math.random() * 4) + 3);
+    const nodes: Point[] = new Array(n);
+    const nodeHeight: number = 45;
+    const nodeWidth: number = 60;
+    const nodeHalfHeight: number = nodeHeight / 2;
+    const nodeHalfWidth: number = nodeWidth / 2;
+    const canvasHalfHeight: number = canvas.height / 2;
+    const canvasWidthSlice: number = canvas.width / n;
+    (function() {
+        /* NOTE: Initialize `nodes` */
+        const k: number = 15;
+        const kHalf: number = k / 2;
+        for (let i: number = 0; i < n; i++) {
+            const xOrigin: number =
+                (canvasWidthSlice * (i + 0.5)) - nodeHalfWidth;
+            nodes[i] = {
+                xOrigin,
+                x: xOrigin + (Math.random() * k) - kHalf,
+                y: canvasHalfHeight - nodeHalfHeight + (Math.random() * k) -
+                    kHalf,
+            };
+        }
+    })();
+    (function() {
+        /* NOTE: Draw everything */
+        const ctx: CanvasRenderingContext2D =
+            canvas.getContext("2d") as CanvasRenderingContext2D;
+        ctx.imageSmoothingEnabled = true;
+        const color: string = "hsl(0, 0%, 20%)";
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 1.65;
+        (function() {
+            /* NOTE: Draw `nodes` surroundings */
+            const yOver: number = canvasHalfHeight - nodeHeight;
+            const yUnder: number = canvasHalfHeight + nodeHeight;
+            ctx.setLineDash([2.25, 3]);
+            const canvasPadWidth: number = 10;
+            ctx.strokeRect(canvasPadWidth, yUnder,
+                           canvasWidthSlice - (canvasPadWidth * 2),
+                           yOver - yUnder);
+            ctx.beginPath();
+            for (let i: number = 1; i < n; i++) {
+                const node: Point = nodes[i];
+                const xOriginOffset: number = node.xOrigin + nodeWidth;
+                ctx.moveTo(node.xOrigin, yOver);
+                ctx.lineTo(xOriginOffset, yOver);
+                ctx.moveTo(node.xOrigin, yUnder);
+                ctx.lineTo(xOriginOffset, yUnder);
+            }
+            ctx.stroke();
+        })();
+        (function() {
+            /* NOTE: Draw `nodes` rects and pointers */
+            const radius: number = 2.5;
+            const twicePi: number = Math.PI * 2;
+            const nodeThreeQuarterWidth: number = nodeWidth * 0.75;
+            ctx.setLineDash([]);
+            ctx.beginPath();
+            for (let i: number = 1; i < n; i++) {
+                const nodeFrom: Point = nodes[i - 1];
+                const nodeTo: Point = nodes[i];
+                const xFrom: number = nodeFrom.x + nodeThreeQuarterWidth;
+                const yFrom: number = nodeFrom.y + nodeHalfHeight;
+                const yTo: number = nodeTo.y + nodeHalfHeight;
+                ctx.strokeRect(nodeFrom.x, nodeFrom.y, nodeWidth, nodeHeight);
+                ctx.moveTo(xFrom + radius, yFrom);
+                ctx.arc(xFrom, yFrom, radius, 0, twicePi);
+                ctx.moveTo(nodeTo.x + radius, yTo);
+                ctx.arc(nodeTo.x, yTo, radius, 0, twicePi);
+                ctx.moveTo(xFrom, yFrom);
+                ctx.lineTo(nodeTo.x, yTo);
+            }
+            ctx.stroke();
+            ctx.fill();
+        })();
+        (function() {
+            /* NOTE: Draw `nodes` separator details */
+            ctx.setLineDash([1, 2]);
+            ctx.beginPath();
+            const m: number = n - 1;
+            for (let i: number = 0; i < m; i++) {
+                const node: Point = nodes[i];
+                const xSep: number = node.x + nodeHalfWidth;
+                ctx.moveTo(xSep, node.y);
+                ctx.lineTo(xSep, node.y + nodeHeight);
+            }
+            ctx.stroke();
+        })();
+    })();
+}
+
+main();
