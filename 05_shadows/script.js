68f6d9c6635f6353e5de0f4bdfe21c640e911497
albertgoncalves <goncalves.albert@gmail.com>
2020-04-09 21:16:12 -0400

    it works!

---
+++ 05_shadows/script.js
@@ -0,0 +1,319 @@
+(function () {
+    var defines = {};
+    var entry = [null];
+    function define(name, dependencies, factory) {
+        defines[name] = { dependencies: dependencies, factory: factory };
+        entry[0] = name;
+    }
+    define("require", ["exports"], function (exports) {
+        Object.defineProperty(exports, "__cjsModule", { value: true });
+        Object.defineProperty(exports, "default", { value: function (name) { return resolve(name); } });
+    });
+    "use strict";
+    var CANVAS_SCALE = 4;
+    var DARK_GRAY = 128;
+    var WHITE = 255;
+    var OPAQUE = 255;
+    var TRANSPARENT = 0;
+    var RADIUS = 47;
+    var RADIUS_SQUARED = RADIUS * RADIUS;
+    var K = 0.4;
+    function setVerticalLine(buffer, width, x, yStart, yEnd) {
+        var start = (yStart * width) + x;
+        var end = (yEnd * width) + x;
+        for (var i = start; i <= end; i += width) {
+            buffer[i] = DARK_GRAY;
+        }
+    }
+    function setHorizontalLine(buffer, width, xStart, xEnd, y) {
+        var yWidth = y * width;
+        var start = yWidth + xStart;
+        var end = yWidth + xEnd;
+        for (var i = start; i <= end; i++) {
+            buffer[i] = DARK_GRAY;
+        }
+    }
+    function setImage(ctx, image, buffer, mask) {
+        for (var i = buffer.length; 0 <= i; i--) {
+            var color = buffer[i];
+            var index = i << 2;
+            image.data[index] = color;
+            image.data[index + 1] = color;
+            image.data[index + 2] = color;
+            image.data[index + 3] = mask[i];
+        }
+        ctx.putImageData(image, 0, 0);
+    }
+    function getBlocked(buffer, position, x, y) {
+        return ((x < 0) || (y < 0) || (position.width <= x) ||
+            (position.height <= y) ||
+            (buffer[(position.width * y) + x] !== WHITE));
+    }
+    function setMaskColRow(mask, buffer, position, octal) {
+        if (octal.slopeStart < octal.slopeEnd) {
+            return;
+        }
+        var nextStart = octal.slopeStart;
+        var yEnd = -RADIUS - 1;
+        for (var dY = -octal.start; yEnd < dY; dY--) {
+            var blocked = false;
+            var y = position.y + (dY * octal.yMult);
+            var yDelta = y - position.y;
+            var yDeltaSquared = yDelta * yDelta;
+            var yWidth = y * position.width;
+            for (var dX = dY - 1; dX < 1; dX++) {
+                var rSlope = (dX + K) / (dY - K);
+                if (octal.slopeStart < rSlope) {
+                    continue;
+                }
+                var lSlope = (dX - K) / (dY + K);
+                if (lSlope < octal.slopeEnd) {
+                    break;
+                }
+                var x = position.x + (dX * octal.xMult);
+                var xDelta = x - position.x;
+                if (((xDelta * xDelta) + yDeltaSquared) < RADIUS_SQUARED) {
+                    if ((0 <= x) && (x < position.width) && (0 <= y) &&
+                        (y < position.height)) {
+                        if (octal.xMult === 1) {
+                            mask[yWidth + x] = OPAQUE;
+                        }
+                        else {
+                            mask[yWidth + x] = OPAQUE;
+                        }
+                    }
+                }
+                if (blocked) {
+                    if (getBlocked(buffer, position, x, y)) {
+                        nextStart = rSlope;
+                        continue;
+                    }
+                    else {
+                        blocked = false;
+                        octal.slopeStart = nextStart;
+                    }
+                }
+                else {
+                    if ((getBlocked(buffer, position, x, y)) && (-dY < RADIUS)) {
+                        blocked = true;
+                        setMaskColRow(mask, buffer, position, {
+                            xMult: octal.xMult,
+                            yMult: octal.yMult,
+                            start: -dY + 1,
+                            slopeStart: nextStart,
+                            slopeEnd: lSlope
+                        });
+                        nextStart = rSlope;
+                    }
+                }
+            }
+            if (blocked) {
+                break;
+            }
+        }
+    }
+    function setMaskRowCol(mask, buffer, position, octal) {
+        if (octal.slopeStart < octal.slopeEnd) {
+            return;
+        }
+        var nextStart = octal.slopeStart;
+        var xEnd = -RADIUS - 1;
+        for (var dX = -octal.start; xEnd < dX; dX--) {
+            var blocked = false;
+            var x = position.x + (dX * octal.xMult);
+            var xDelta = x - position.x;
+            var xDeltaSquared = xDelta * xDelta;
+            for (var dY = dX - 1; dY < 1; dY++) {
+                var rSlope = (dY + K) / (dX - K);
+                if (octal.slopeStart < rSlope) {
+                    continue;
+                }
+                var lSlope = (dY - K) / (dX + K);
+                if (lSlope < octal.slopeEnd) {
+                    break;
+                }
+                var y = position.y + (dY * octal.yMult);
+                var yDelta = y - position.y;
+                var yWidth = y * position.width;
+                if ((xDeltaSquared + (yDelta * yDelta)) < RADIUS_SQUARED) {
+                    if ((0 <= x) && (x < position.width) && (0 <= y) &&
+                        (y < position.height)) {
+                        if (octal.yMult === 1) {
+                            mask[yWidth + x] = OPAQUE;
+                        }
+                        else {
+                            mask[yWidth + x] = OPAQUE;
+                        }
+                    }
+                }
+                if (blocked) {
+                    if (getBlocked(buffer, position, x, y)) {
+                        nextStart = rSlope;
+                        continue;
+                    }
+                    else {
+                        blocked = false;
+                        octal.slopeStart = nextStart;
+                    }
+                }
+                else {
+                    if ((getBlocked(buffer, position, x, y)) && (-dX < RADIUS)) {
+                        blocked = true;
+                        setMaskRowCol(mask, buffer, position, {
+                            xMult: octal.xMult,
+                            yMult: octal.yMult,
+                            start: -dX + 1,
+                            slopeStart: octal.slopeStart,
+                            slopeEnd: lSlope
+                        });
+                        nextStart = rSlope;
+                    }
+                }
+            }
+            if (blocked) {
+                break;
+            }
+        }
+    }
+    function setMask(mask, buffer, position) {
+        mask.fill(TRANSPARENT);
+        mask[(position.y * position.width) + position.x] = OPAQUE;
+        setMaskColRow(mask, buffer, position, {
+            xMult: 1,
+            yMult: 1,
+            start: 1,
+            slopeStart: 1.0,
+            slopeEnd: 0.0
+        });
+        setMaskColRow(mask, buffer, position, {
+            xMult: 1,
+            yMult: -1,
+            start: 1,
+            slopeStart: 1.0,
+            slopeEnd: 0.0
+        });
+        setMaskColRow(mask, buffer, position, {
+            xMult: -1,
+            yMult: 1,
+            start: 1,
+            slopeStart: 1.0,
+            slopeEnd: 0.0
+        });
+        setMaskColRow(mask, buffer, position, {
+            xMult: -1,
+            yMult: -1,
+            start: 1,
+            slopeStart: 1.0,
+            slopeEnd: 0.0
+        });
+        setMaskRowCol(mask, buffer, position, {
+            xMult: 1,
+            yMult: 1,
+            start: 1,
+            slopeStart: 1.0,
+            slopeEnd: 0.0
+        });
+        setMaskRowCol(mask, buffer, position, {
+            xMult: 1,
+            yMult: -1,
+            start: 1,
+            slopeStart: 1.0,
+            slopeEnd: 0.0
+        });
+        setMaskRowCol(mask, buffer, position, {
+            xMult: -1,
+            yMult: 1,
+            start: 1,
+            slopeStart: 1.0,
+            slopeEnd: 0.0
+        });
+        setMaskRowCol(mask, buffer, position, {
+            xMult: -1,
+            yMult: -1,
+            start: 1,
+            slopeStart: 1.0,
+            slopeEnd: 0.0
+        });
+    }
+    window.onload = function () {
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.imageSmoothingEnabled = false;
+        var position = {
+            x: 15,
+            y: 15,
+            width: canvas.width,
+            height: canvas.height
+        };
+        var n = position.width * position.height;
+        var image = ctx.createImageData(position.width, position.height);
+        var buffer = new Uint8ClampedArray(n);
+        var mask = new Uint8ClampedArray(n);
+        buffer.fill(WHITE);
+        {
+            setVerticalLine(buffer, position.width, 6, 10, 20);
+            setVerticalLine(buffer, position.width, 25, 10, 20);
+            setHorizontalLine(buffer, position.width, 7, 24, 5);
+            setHorizontalLine(buffer, position.width, 7, 24, 26);
+            buffer[(position.y * position.width) + position.x] = DARK_GRAY;
+            setMask(mask, buffer, position);
+            setImage(ctx, image, buffer, mask);
+        }
+        canvas.addEventListener("mousedown", function (event) {
+            var x = (event.x + window.pageXOffset - canvas.offsetLeft) >> CANVAS_SCALE;
+            var y = (event.y + window.pageYOffset - canvas.offsetTop) >> CANVAS_SCALE;
+            var index = (y * position.width) + x;
+            if (buffer[index] === WHITE) {
+                buffer[(position.y * position.width) + position.x] = WHITE;
+                buffer[index] = DARK_GRAY;
+                position.x = x;
+                position.y = y;
+                setMask(mask, buffer, position);
+                setImage(ctx, image, buffer, mask);
+            }
+        });
+    };
+    
+    'marker:resolver';
+
+    function get_define(name) {
+        if (defines[name]) {
+            return defines[name];
+        }
+        else if (defines[name + '/index']) {
+            return defines[name + '/index'];
+        }
+        else {
+            var dependencies = ['exports'];
+            var factory = function (exports) {
+                try {
+                    Object.defineProperty(exports, "__cjsModule", { value: true });
+                    Object.defineProperty(exports, "default", { value: require(name) });
+                }
+                catch (_a) {
+                    throw Error(['module "', name, '" not found.'].join(''));
+                }
+            };
+            return { dependencies: dependencies, factory: factory };
+        }
+    }
+    var instances = {};
+    function resolve(name) {
+        if (instances[name]) {
+            return instances[name];
+        }
+        if (name === 'exports') {
+            return {};
+        }
+        var define = get_define(name);
+        instances[name] = {};
+        var dependencies = define.dependencies.map(function (name) { return resolve(name); });
+        define.factory.apply(define, dependencies);
+        var exports = dependencies[define.dependencies.indexOf('exports')];
+        instances[name] = (exports['__cjsModule']) ? exports["default"] : exports;
+        return instances[name];
+    }
+    if (entry[0] !== null) {
+        return resolve(entry[0]);
+    }
+})();
---
+++ 05_shadows/src/main.ts
@@ -0,0 +1,296 @@
+const CANVAS_SCALE: number = 4;
+
+const DARK_GRAY: number = 128;
+const WHITE: number = 255;
+
+const OPAQUE: number = 255;
+const TRANSPARENT: number = 0;
+
+const RADIUS: number = 47;
+const RADIUS_SQUARED: number = RADIUS * RADIUS;
+
+const K: number = 0.4;
+
+interface Position_ {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+interface Octal {
+    xMult: number;
+    yMult: number;
+    start: number;
+    slopeStart: number;
+    slopeEnd: number;
+}
+
+function setVerticalLine(buffer: Uint8ClampedArray, width: number, x: number,
+                         yStart: number, yEnd: number) {
+    const start: number = (yStart * width) + x;
+    const end: number = (yEnd * width) + x;
+    for (let i: number = start; i <= end; i += width) {
+        buffer[i] = DARK_GRAY;
+    }
+}
+
+function setHorizontalLine(buffer: Uint8ClampedArray, width: number,
+                           xStart: number, xEnd: number, y: number) {
+    const yWidth: number = y * width;
+    const start: number = yWidth + xStart;
+    const end: number = yWidth + xEnd;
+    for (let i: number = start; i <= end; i++) {
+        buffer[i] = DARK_GRAY;
+    }
+}
+
+function setImage(ctx: CanvasRenderingContext2D, image: ImageData,
+                  buffer: Uint8ClampedArray, mask: Uint8ClampedArray) {
+    for (let i: number = buffer.length; 0 <= i; i--) {
+        const color: number = buffer[i];
+        const index: number = i << 2;
+        image.data[index] = color;
+        image.data[index + 1] = color;
+        image.data[index + 2] = color;
+        image.data[index + 3] = mask[i];
+    }
+    ctx.putImageData(image, 0, 0);
+}
+
+function getBlocked(buffer: Uint8ClampedArray, position: Position_, x: number,
+                    y: number) {
+    return ((x < 0) || (y < 0) || (position.width <= x) ||
+            (position.height <= y) ||
+            (buffer[(position.width * y) + x] !== WHITE));
+}
+
+function setMaskColRow(mask: Uint8ClampedArray, buffer: Uint8ClampedArray,
+                       position: Position_, octal: Octal) {
+    if (octal.slopeStart < octal.slopeEnd) {
+        return;
+    }
+    let nextStart: number = octal.slopeStart;
+    const yEnd: number = -RADIUS - 1;
+    for (let dY: number = -octal.start; yEnd < dY; dY--) {
+        let blocked: boolean = false;
+        const y: number = position.y + (dY * octal.yMult);
+        const yDelta: number = y - position.y;
+        const yDeltaSquared: number = yDelta * yDelta;
+        const yWidth: number = y * position.width;
+        for (let dX: number = dY - 1; dX < 1; dX++) {
+            const rSlope: number = (dX + K) / (dY - K);
+            if (octal.slopeStart < rSlope) {
+                continue;
+            }
+            const lSlope: number = (dX - K) / (dY + K);
+            if (lSlope < octal.slopeEnd) {
+                break;
+            }
+            const x: number = position.x + (dX * octal.xMult);
+            const xDelta: number = x - position.x;
+            if (((xDelta * xDelta) + yDeltaSquared) < RADIUS_SQUARED) {
+                if ((0 <= x) && (x < position.width) && (0 <= y) &&
+                    (y < position.height)) {
+                    if (octal.xMult === 1) {
+                        mask[yWidth + x] = OPAQUE;
+                    } else {
+                        mask[yWidth + x] = OPAQUE;
+                    }
+                }
+            }
+            if (blocked) {
+                if (getBlocked(buffer, position, x, y)) {
+                    nextStart = rSlope;
+                    continue;
+                } else {
+                    blocked = false;
+                    octal.slopeStart = nextStart;
+                }
+            } else {
+                if ((getBlocked(buffer, position, x, y)) && (-dY < RADIUS)) {
+                    blocked = true;
+                    setMaskColRow(mask, buffer, position, {
+                        xMult: octal.xMult,
+                        yMult: octal.yMult,
+                        start: -dY + 1,
+                        slopeStart: nextStart,
+                        slopeEnd: lSlope,
+                    });
+                    nextStart = rSlope;
+                }
+            }
+        }
+        if (blocked) {
+            break;
+        }
+    }
+}
+
+function setMaskRowCol(mask: Uint8ClampedArray, buffer: Uint8ClampedArray,
+                       position: Position_, octal: Octal) {
+    if (octal.slopeStart < octal.slopeEnd) {
+        return;
+    }
+    let nextStart: number = octal.slopeStart;
+    const xEnd: number = -RADIUS - 1;
+    for (let dX: number = -octal.start; xEnd < dX; dX--) {
+        let blocked: boolean = false;
+        const x: number = position.x + (dX * octal.xMult);
+        const xDelta: number = x - position.x;
+        const xDeltaSquared: number = xDelta * xDelta;
+        for (let dY: number = dX - 1; dY < 1; dY++) {
+            const rSlope: number = (dY + K) / (dX - K);
+            if (octal.slopeStart < rSlope) {
+                continue;
+            }
+            const lSlope: number = (dY - K) / (dX + K);
+            if (lSlope < octal.slopeEnd) {
+                break;
+            }
+            const y: number = position.y + (dY * octal.yMult);
+            const yDelta: number = y - position.y;
+            const yWidth: number = y * position.width;
+            if ((xDeltaSquared + (yDelta * yDelta)) < RADIUS_SQUARED) {
+                if ((0 <= x) && (x < position.width) && (0 <= y) &&
+                    (y < position.height)) {
+                    if (octal.yMult === 1) {
+                        mask[yWidth + x] = OPAQUE;
+                    } else {
+                        mask[yWidth + x] = OPAQUE;
+                    }
+                }
+            }
+            if (blocked) {
+                if (getBlocked(buffer, position, x, y)) {
+                    nextStart = rSlope;
+                    continue;
+                } else {
+                    blocked = false;
+                    octal.slopeStart = nextStart;
+                }
+            } else {
+                if ((getBlocked(buffer, position, x, y)) && (-dX < RADIUS)) {
+                    blocked = true;
+                    setMaskRowCol(mask, buffer, position, {
+                        xMult: octal.xMult,
+                        yMult: octal.yMult,
+                        start: -dX + 1,
+                        slopeStart: octal.slopeStart,
+                        slopeEnd: lSlope,
+                    });
+                    nextStart = rSlope;
+                }
+            }
+        }
+        if (blocked) {
+            break;
+        }
+    }
+}
+
+function setMask(mask: Uint8ClampedArray, buffer: Uint8ClampedArray,
+                 position: Position_) {
+    mask.fill(TRANSPARENT);
+    mask[(position.y * position.width) + position.x] = OPAQUE;
+    setMaskColRow(mask, buffer, position, {
+        xMult: 1,
+        yMult: 1,
+        start: 1,
+        slopeStart: 1.0,
+        slopeEnd: 0.0,
+    });
+    setMaskColRow(mask, buffer, position, {
+        xMult: 1,
+        yMult: -1,
+        start: 1,
+        slopeStart: 1.0,
+        slopeEnd: 0.0,
+    });
+    setMaskColRow(mask, buffer, position, {
+        xMult: -1,
+        yMult: 1,
+        start: 1,
+        slopeStart: 1.0,
+        slopeEnd: 0.0,
+    });
+    setMaskColRow(mask, buffer, position, {
+        xMult: -1,
+        yMult: -1,
+        start: 1,
+        slopeStart: 1.0,
+        slopeEnd: 0.0,
+    });
+    setMaskRowCol(mask, buffer, position, {
+        xMult: 1,
+        yMult: 1,
+        start: 1,
+        slopeStart: 1.0,
+        slopeEnd: 0.0,
+    });
+    setMaskRowCol(mask, buffer, position, {
+        xMult: 1,
+        yMult: -1,
+        start: 1,
+        slopeStart: 1.0,
+        slopeEnd: 0.0,
+    });
+    setMaskRowCol(mask, buffer, position, {
+        xMult: -1,
+        yMult: 1,
+        start: 1,
+        slopeStart: 1.0,
+        slopeEnd: 0.0,
+    });
+    setMaskRowCol(mask, buffer, position, {
+        xMult: -1,
+        yMult: -1,
+        start: 1,
+        slopeStart: 1.0,
+        slopeEnd: 0.0,
+    });
+}
+
+window.onload = function() {
+    const canvas: HTMLCanvasElement =
+        document.getElementById("canvas") as HTMLCanvasElement;
+    const ctx: CanvasRenderingContext2D =
+        canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.imageSmoothingEnabled = false;
+    const position: Position_ = {
+        x: 15,
+        y: 15,
+        width: canvas.width,
+        height: canvas.height,
+    };
+    const n: number = position.width * position.height;
+    const image: ImageData =
+        ctx.createImageData(position.width, position.height);
+    const buffer: Uint8ClampedArray = new Uint8ClampedArray(n);
+    const mask: Uint8ClampedArray = new Uint8ClampedArray(n);
+    buffer.fill(WHITE);
+    {
+        setVerticalLine(buffer, position.width, 6, 10, 20);
+        setVerticalLine(buffer, position.width, 25, 10, 20);
+        setHorizontalLine(buffer, position.width, 7, 24, 5);
+        setHorizontalLine(buffer, position.width, 7, 24, 26);
+        buffer[(position.y * position.width) + position.x] = DARK_GRAY;
+        setMask(mask, buffer, position);
+        setImage(ctx, image, buffer, mask);
+    }
+    canvas.addEventListener("mousedown", function(event: MouseEvent) {
+        const x: number =
+            (event.x + window.pageXOffset - canvas.offsetLeft) >> CANVAS_SCALE;
+        const y: number =
+            (event.y + window.pageYOffset - canvas.offsetTop) >> CANVAS_SCALE;
+        const index: number = (y * position.width) + x;
+        if (buffer[index] === WHITE) {
+            buffer[(position.y * position.width) + position.x] = WHITE;
+            buffer[index] = DARK_GRAY;
+            position.x = x;
+            position.y = y;
+            setMask(mask, buffer, position);
+            setImage(ctx, image, buffer, mask);
+        }
+    });
+};
