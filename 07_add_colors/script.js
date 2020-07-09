b826e3c663a94dc95dc60aa2c6bf87e39aa51e4b
Albert Goncalves <goncalves.albert@gmail.com>
2020-07-08 23:31:22 -0400

    low-res

--- 07_add_colors/script.js
+++ 07_add_colors/script.js
@@ -10,95 +10,84 @@
         Object.defineProperty(exports, "default", { value: function (name) { return resolve(name); } });
     });
     "use strict";
-    var GLOBAL = {
-        width: 0,
-        height: 0
-    };
-    function setRect(buffer, rect, color) {
-        for (var y = 0; y < rect.height; ++y) {
-            var offset = ((rect.y + y) << 2) * GLOBAL.width;
-            for (var x = 0; x < rect.width; ++x) {
-                var i = ((rect.x + x) << 2) + offset;
-                buffer.data[i] = Math.min(buffer.data[i] + color.red, 255);
-                buffer.data[i + 1] =
-                    Math.min(buffer.data[i + 1] + color.green, 255);
-                buffer.data[i + 2] =
-                    Math.min(buffer.data[i + 2] + color.blue, 255);
-                buffer.data[i + 3] =
-                    Math.min(buffer.data[i + 3] + color.alpha, 255);
+    var __assign = (this && this.__assign) || function () {
+        __assign = Object.assign || function(t) {
+            for (var s, i = 1, n = arguments.length; i < n; i++) {
+                s = arguments[i];
+                for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                    t[p] = s[p];
             }
-        }
-    }
+            return t;
+        };
+        return __assign.apply(this, arguments);
+    };
     window.onload = function () {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.imageSmoothingEnabled = false;
-        GLOBAL.width = canvas.width;
-        GLOBAL.height = canvas.height;
-        var halfWidth = GLOBAL.width / 2.0;
-        var halfHeight = GLOBAL.height / 2.0;
-        var n = GLOBAL.width * GLOBAL.height * 4;
+        var n = canvas.width * canvas.height * 4;
         if (n === 0) {
             return;
         }
-        var rect = {
-            x: 64,
-            y: 64,
-            width: 128,
-            height: 128
-        };
-        if ((rect.x < 0.0) || (GLOBAL.width <= (rect.x + rect.width)) ||
-            (rect.y < 0.0) || (GLOBAL.height <= (rect.y + rect.height)) ||
-            (rect.width < 0.0) || (halfWidth <= rect.width) ||
-            (rect.height < 0.0) || (halfHeight <= rect.height)) {
-            return;
-        }
-        var color = {
-            red: 6,
-            green: 9,
-            blue: 12,
-            alpha: 8
-        };
-        var decay = 1;
-        var spread = 32.0;
+        var buffer = ctx.createImageData(canvas.width, canvas.height);
+        var decay = 2;
+        var spread = 4.0;
         var halfSpread = spread / 2.0;
-        var buffer = ctx.createImageData(GLOBAL.width, GLOBAL.height);
+        var epsilon = 0.015;
+        var initRect = {
+            x: 20,
+            y: 20,
+            width: 24,
+            height: 24
+        };
+        var rect = __assign({}, initRect);
+        var color = {
+            red: 12,
+            green: 18,
+            blue: 24,
+            alpha: 4
+        };
         function loop() {
-            {
-                var x = rect.x + (Math.random() * spread) - halfSpread;
-                while ((x < 0.0) || (GLOBAL.width <= (x + rect.width))) {
-                    x = rect.x + (Math.random() * spread) - halfSpread;
-                }
-                rect.x = x;
-            }
-            {
-                var y = rect.y + (Math.random() * spread) - halfSpread;
-                while ((y < 0.0) || (GLOBAL.height <= (y + rect.height))) {
-                    y = rect.y + (Math.random() * spread) - halfSpread;
-                }
-                rect.y = y;
-            }
-            {
-                var width = rect.width + (Math.random() * spread) - halfSpread;
-                while ((width < 0.0) || (halfWidth <= width)) {
-                    width = rect.width + (Math.random() * spread) - halfSpread;
-                }
-                rect.width = width;
-            }
-            {
-                var height = rect.height + (Math.random() * spread) - halfSpread;
-                while ((height < 0.0) || (halfHeight <= height)) {
-                    height = rect.height + (Math.random() * spread) - halfSpread;
-                }
-                rect.height = height;
-            }
+            rect.x += Math.round((Math.random() * spread) - halfSpread - epsilon);
+            rect.y += Math.round((Math.random() * spread) - halfSpread - epsilon);
             for (var i = 0; i < n; i += 4) {
                 buffer.data[i] = Math.max(buffer.data[i] - decay, 0);
                 buffer.data[i + 1] = Math.max(buffer.data[i + 1] - decay, 0);
                 buffer.data[i + 2] = Math.max(buffer.data[i + 2] - decay, 0);
                 buffer.data[i + 3] = Math.max(buffer.data[i + 3] - decay, 0);
             }
-            setRect(buffer, rect, color);
+            var reset = true;
+            for (var y = rect.y + rect.height - 1; rect.y <= y; --y) {
+                if (y < 0) {
+                    break;
+                }
+                if (canvas.height <= y) {
+                    continue;
+                }
+                var offset = (y << 2) * canvas.width;
+                for (var x = rect.x + rect.width - 1; rect.x <= x; --x) {
+                    if (x < 0) {
+                        break;
+                    }
+                    if (canvas.width <= x) {
+                        continue;
+                    }
+                    var i = (x << 2) + offset;
+                    buffer.data[i] = Math.min(buffer.data[i] + color.red, 255);
+                    buffer.data[i + 1] =
+                        Math.min(buffer.data[i + 1] + color.green, 255);
+                    buffer.data[i + 2] =
+                        Math.min(buffer.data[i + 2] + color.blue, 255);
+                    buffer.data[i + 3] =
+                        Math.min(buffer.data[i + 3] + color.alpha, 255);
+                    if (reset) {
+                        reset = false;
+                    }
+                }
+            }
+            if (reset) {
+                rect = __assign({}, initRect);
+            }
             ctx.putImageData(buffer, 0, 0);
             window.requestAnimationFrame(loop);
         }
--- 07_add_colors/src/main.ts
+++ 07_add_colors/src/main.ts
@@ -1,8 +1,3 @@
-interface Global {
-    width: number;
-    height: number;
-}
-
 interface Color {
     red: number;
     green: number;
@@ -17,100 +12,79 @@ interface Rect {
     height: number;
 }
 
-const GLOBAL: Global = {
-    width: 0,
-    height: 0,
-};
-
-function setRect(buffer: ImageData, rect: Rect, color: Color) {
-    for (let y: number = 0; y < rect.height; ++y) {
-        const offset: number = ((rect.y + y) << 2) * GLOBAL.width;
-        for (let x: number = 0; x < rect.width; ++x) {
-            const i: number = ((rect.x + x) << 2) + offset;
-            buffer.data[i] = Math.min(buffer.data[i] + color.red, 255);
-            buffer.data[i + 1] =
-                Math.min(buffer.data[i + 1] + color.green, 255);
-            buffer.data[i + 2] =
-                Math.min(buffer.data[i + 2] + color.blue, 255);
-            buffer.data[i + 3] =
-                Math.min(buffer.data[i + 3] + color.alpha, 255);
-        }
-    }
-}
-
 window.onload = function() {
     const canvas: HTMLCanvasElement =
         document.getElementById("canvas") as HTMLCanvasElement;
     const ctx: CanvasRenderingContext2D =
         canvas.getContext("2d") as CanvasRenderingContext2D;
     ctx.imageSmoothingEnabled = false;
-    GLOBAL.width = canvas.width;
-    GLOBAL.height = canvas.height;
-    const halfWidth: number = GLOBAL.width / 2.0;
-    const halfHeight: number = GLOBAL.height / 2.0;
-    const n: number = GLOBAL.width * GLOBAL.height * 4;
+    const n: number = canvas.width * canvas.height * 4;
     if (n === 0) {
         return;
     }
-    const rect: Rect = {
-        x: 64,
-        y: 64,
-        width: 128,
-        height: 128,
-    };
-    if ((rect.x < 0.0) || (GLOBAL.width <= (rect.x + rect.width)) ||
-        (rect.y < 0.0) || (GLOBAL.height <= (rect.y + rect.height)) ||
-        (rect.width < 0.0) || (halfWidth <= rect.width) ||
-        (rect.height < 0.0) || (halfHeight <= rect.height))
-    {
-        return;
-    }
-    const color: Color = {
-        red: 6,
-        green: 9,
-        blue: 12,
-        alpha: 8,
-    };
-    const decay: number = 1;
-    const spread: number = 32.0;
+    const buffer: ImageData = ctx.createImageData(canvas.width, canvas.height);
+    const decay: number = 2;
+    const spread: number = 4.0;
     const halfSpread: number = spread / 2.0;
-    const buffer: ImageData = ctx.createImageData(GLOBAL.width, GLOBAL.height);
+    const epsilon: number = 0.015;
+    const initRect: Rect = {
+        x: 20,
+        y: 20,
+        width: 24,
+        height: 24,
+    };
+    let rect: Rect = {
+        ...initRect,
+    };
+    const color: Color = {
+        red: 12,
+        green: 18,
+        blue: 24,
+        alpha: 4,
+    };
     function loop() {
-        {
-            let x = rect.x + (Math.random() * spread) - halfSpread;
-            while ((x < 0.0) || (GLOBAL.width <= (x + rect.width))) {
-                x = rect.x + (Math.random() * spread) - halfSpread;
-            }
-            rect.x = x;
-        }
-        {
-            let y = rect.y + (Math.random() * spread) - halfSpread;
-            while ((y < 0.0) || (GLOBAL.height <= (y + rect.height))) {
-                y = rect.y + (Math.random() * spread) - halfSpread;
-            }
-            rect.y = y;
-        }
-        {
-            let width = rect.width + (Math.random() * spread) - halfSpread;
-            while ((width < 0.0) || (halfWidth <= width)) {
-                width = rect.width + (Math.random() * spread) - halfSpread;
-            }
-            rect.width = width;
-        }
-        {
-            let height = rect.height + (Math.random() * spread) - halfSpread;
-            while ((height < 0.0) || (halfHeight <= height)) {
-                height = rect.height + (Math.random() * spread) - halfSpread;
-            }
-            rect.height = height;
-        }
+        rect.x += Math.round((Math.random() * spread) - halfSpread - epsilon);
+        rect.y += Math.round((Math.random() * spread) - halfSpread - epsilon);
         for (let i: number = 0; i < n; i += 4) {
             buffer.data[i] = Math.max(buffer.data[i] - decay, 0);
             buffer.data[i + 1] = Math.max(buffer.data[i + 1] - decay, 0);
             buffer.data[i + 2] = Math.max(buffer.data[i + 2] - decay, 0);
             buffer.data[i + 3] = Math.max(buffer.data[i + 3] - decay, 0);
         }
-        setRect(buffer, rect, color);
+        let reset: boolean = true;
+        for (let y: number = rect.y + rect.height - 1; rect.y <= y; --y) {
+            if (y < 0) {
+                break;
+            }
+            if (canvas.height <= y) {
+                continue;
+            }
+            const offset: number = (y << 2) * canvas.width;
+            for (let x: number = rect.x + rect.width - 1; rect.x <= x; --x) {
+                if (x < 0) {
+                    break;
+                }
+                if (canvas.width <= x) {
+                    continue;
+                }
+                const i: number = (x << 2) + offset;
+                buffer.data[i] = Math.min(buffer.data[i] + color.red, 255);
+                buffer.data[i + 1] =
+                    Math.min(buffer.data[i + 1] + color.green, 255);
+                buffer.data[i + 2] =
+                    Math.min(buffer.data[i + 2] + color.blue, 255);
+                buffer.data[i + 3] =
+                    Math.min(buffer.data[i + 3] + color.alpha, 255);
+                if (reset) {
+                    reset = false;
+                }
+            }
+        }
+        if (reset) {
+            rect = {
+                ...initRect,
+            };
+        }
         ctx.putImageData(buffer, 0, 0);
         window.requestAnimationFrame(loop);
     }
